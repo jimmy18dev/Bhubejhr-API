3e1ef7b37fc7730c13d99bb927d46854a1ccda36
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2017-08-24 15:09:32 +0700

    ทดสอบ API Service

--- service.php
+++ service.php
@@ -3,22 +3,39 @@ require_once 'autoload.php';
 header("Content-type: text/json");
 
 $api 		= new Api;
-$queries 	= new Queries;
+$app 		= new App;
+
 $qid 		= $_GET['qid'];
+$token 		= $_GET['token'];
+$analytics = new Analytics;
 
-$query 		= $queries->getQuery($qid);
-$dataset 	= $queries->process($query,$_GET);
-$api->successMessage('CALLING API SERVICE '.$query,'',$dataset);
+if(!empty($token)){
+	$app_id = $app->authentication($token);
+	if(empty($app_id)){ $api->errorMessage('Invalid Token!'); exit(); }
+}else{
+	$api->errorMessage('Token not found!');
+	exit();
+}
 
-// API Request $_POST
-// switch ($_GET['calling']) {
-// 	case 'get':
-		
-// 		break;
-// 	default:
-// 		$api->errorMessage('COMMENT POST API ERROR!');
-// 		break;
-// }
+if(!empty($_GET)){
+	switch ($_GET['action']) {
+		case 'list_patient':
+			$dataset = $analytics->listPatient();
+			$api->exportJson('ทดสอบ',$dataset);
+			// $qid = $queries->createQuery($user_id,$name,$description,$query,$url_example,$type);
+			// $api->successMessage('New Query Created.','',floatval($qid),'');
+			break;
+		case 'delete':
+			// $queries->deleteQuery($qid);
+			// $api->successMessage('Query Deleted.','',floatval($qid),'');
+			break;
+		default:
+			$api->errorMessage('API no action!');
+			break;
+	}
+}else{
+	$api->errorMessage('Invalid Signature or API not found!');
+}
 
 exit();
 ?>
