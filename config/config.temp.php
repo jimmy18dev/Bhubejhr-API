27e9c9e241edf4fdf8d49262a00982b9e3eafd3e
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2017-08-24 13:50:51 +0700

    Add Config File.

---
+++ config/config.temp.php
@@ -0,0 +1,16 @@
+<?php
+// DATABASE CONNECTION SEUTUP
+define("DB_HOST" 	,"localhost");
+define("DB_NAME" 	,"xxx");
+define("DB_USER"	,"xxx");
+define("DB_PASS" 	,"xxx");
+
+define("DOMAIN" 	,"http://".$_SERVER['SERVER_NAME']);
+
+// SITE SETUP
+define("TITLE" 		,'Bhubejhr Monitor');
+define("DESCRIPTION",'Internet of Things (IoT) monitoring โรงพยาบาลเจ้าพระยาอภัยภูเบศร');
+
+// SITE SECRET KEY
+define("SECRET_KEY"	,'dinsorsee');
+?>
