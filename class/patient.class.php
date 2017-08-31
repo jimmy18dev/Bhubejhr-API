f8626e78e8401adb347d6be49a262a6e5a76d07b
reno1723 <reno1723@hotmail.com>
2017-08-31 15:22:12 +0700

    patient&visit
    เพิ่มรายละเอียดเลือกเฉพาะของผุ้ป่วย
    และรายละเอียดแต่ละ visit

--- class/patient.class.php
+++ class/patient.class.php
@@ -9,7 +9,9 @@ class Patient{
     }
 
     public function get($cid){
-        $this->db->query('SELECT * FROM patient WHERE cid = :cid');
+        $this->db->query('SELECT p.hn as  hn,p.fname as fname,p.lname as lname,p.dob as dob,if(p.gender = 1,"ชาย","หญิง") as gender,p.cid as cid,p.nationality as nationality,CONCAT(pa.address," ม.",pa.moo," ",pa.subdistrictname," ",pa.districtname," ",pa.provincename) as Address,p.tel as tel,p.blood as blood FROM patient p 
+left JOIN view_PatAddressType1 pa ON p.hn = pa.hn 
+WHERE cid = :cid');
         $this->db->bind(':cid',$cid);
         $this->db->execute();
         return $dataset = $this->db->single();
--- class/visit.class.php
+++ class/visit.class.php
@@ -12,7 +12,7 @@ class Visit{
         
         if(empty($hn)) return false;
 
-        $this->db->query('SELECT * FROM visit WHERE hn = :hn LIMIT 200');
+        $this->db->query('SELECT *,CONCAT(YEAR(visitdate)-1957,lpad(MONTH(visitdate),2,"0"),lpad(visitno,5,"0")) as  vn FROM visit WHERE hn = :hn  order by visitdate desc LIMIT 500');
         $this->db->bind(':hn',$hn);
         $this->db->execute();
         return $dataset = $this->db->resultset();
