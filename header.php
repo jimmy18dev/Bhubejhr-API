cd31fc2301d5bd124cb844b59b99d2f2d5ff9d83
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2017-08-31 13:51:08 +0700

    API TEST BUTTON
    - เพิ่มปุ่ม API TEST

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 	<a href="index.php" class="nav <?php echo ($currentPage == 'apps'?'-active':'');?>">Apps</a>
 	<a href="queries.php" class="nav <?php echo ($currentPage == 'queries'?'-active':'');?>">Queries</a>
 
+	<a href="http://jhos.api/service.php?token=e90dab3ba52d42dbd133c416c5293f0ad&action=list_patient&qid=1" class="btn" target="_blank">API TEST</a>
+
 	<?php if($currentPage == 'apps'){?>
 	<a href="app-form.php" class="btn">New App</a>
 	<?php }else if($currentPage == 'queries'){?>
