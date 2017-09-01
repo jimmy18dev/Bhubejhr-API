c1c54be9d9c88bca9ed47329bb7bf2cd09243693
reno1723 <reno1723@hotmail.com>
2017-09-01 11:48:25 +0700

    edit add hn

--- class/visit.class.php
+++ class/visit.class.php
@@ -12,7 +12,12 @@ class Visit{
         
         if(empty($hn)) return false;
 
-        $this->db->query('SELECT *,CONCAT(YEAR(visitdate)-1957,lpad(MONTH(visitdate),2,"0"),lpad(visitno,5,"0")) as  vn FROM visit WHERE hn = :hn  order by visitdate desc LIMIT 500');
+        $this->db->query('SELECT v.*,CONCAT(YEAR(visitdate)-1957,lpad(MONTH(visitdate),2,"0"),lpad(visitno,5,"0")) as vn,cr.rightname as rightname
+            FROM visit v
+            inner join visit_ins vi on v.id = vi.id
+            inner join c_righthos cr on vi.inshos = cr.rightcode
+            WHERE hn = :hn  
+            order by visitdate desc LIMIT 500');
         $this->db->bind(':hn',$hn);
         $this->db->execute();
         return $dataset = $this->db->resultset();
