ab92f4a3dae828ca09c1a9603bf22439d06c3fc9
reno1723 <reno1723@hotmail.com>
2017-09-03 14:13:14 +0700

    แพ้ยา

--- class/patient.class.php
+++ class/patient.class.php
@@ -9,8 +9,9 @@ class Patient{
     }
 
     public function get($cid){
-        $this->db->query('SELECT p.hn as  hn,p.fname as fname,p.lname as lname,p.dob as dob,if(p.gender = 1,"ชาย","หญิง") as gender,p.cid as cid,cn.nationname as nationality,CONCAT(pa.address," ม.",pa.moo," ",pa.subdistrictname," ",pa.districtname," ",pa.provincename) as Address,p.tel as tel,cb.bloodgroupname as blood FROM patient p 
+        $this->db->query('SELECT p.hn as  hn,p.fname as fname,p.lname as lname,p.dob as dob,if(p.gender = 1,"ชาย","หญิง") as gender,p.cid as cid,cn.nationname as nationality,CONCAT(pa.address," ม.",pa.moo," ",pa.subdistrictname," ",pa.districtname," ",pa.provincename) as Address,p.tel as tel,cb.bloodgroupname as blood,pd.drug as drugallergy FROM patient p 
 left JOIN view_PatAddressType1 pa ON p.hn = pa.hn  left join c_bloodgroup cb on cb.bloodgroupcode = p.blood left join c_nation cn on p.nationality = cn.nationcode
+LEFT JOIN pt_drugallergy pd ON p.hn = pd.hn 
 WHERE p.cid = :cid or p.hn = :cid');
         $this->db->bind(':cid',$cid);
         $this->db->execute();
