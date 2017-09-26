2a498f8a5461d42ff5c539224693e8b4067011eb
reno1723 <reno1723@hotmail.com>
2017-09-26 17:06:41 +0700

    edit field name
    แก้ ชื่อ จาก IPD เป็นwork เพราะ มีของ OPD ด้วย

--- class/diag_user.class.php
+++ class/diag_user.class.php
@@ -10,7 +10,7 @@ class Diag_user{
 
     public function listDiag_user(){
         
-        $this->db->query('SELECT "IPD",su.uid,su.fname,su.lname,COUNT(di.an) as num
+        $this->db->query('SELECT "IPD" as work,su.uid,su.fname,su.lname,COUNT(di.an) as num
             FROM visit v
             INNER JOIN ipd i ON v.id = i.visitid
             INNER JOIN ipd_diag di ON di.an = i.an
@@ -21,7 +21,7 @@ class Diag_user{
             AND date(DATE_SUB(now(),INTERVAL day(now()) DAY))
             GROUP BY su.uid
             UNION 
-            SELECT "OPD",su.uid,su.fname,su.lname,COUNT(vd.id) as num
+            SELECT "OPD" as work,su.uid,su.fname,su.lname,COUNT(vd.id) as num
             FROM visit v
             INNER JOIN visit_diag vd ON v.id = vd.id
             INNER JOIN sys_user su ON su.uid = vd.userupdate
