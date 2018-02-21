56df79fa3fbffc631d449d1d743c51c0be7726a1
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-02-21 15:45:54 +0700

    add height and weight

--- class/patient/patient.class.php
+++ class/patient/patient.class.php
@@ -11,7 +11,8 @@ class Patient{
     public function get($cid){
         if(empty($cid)) return null;
         
-        $this->db->query('SELECT p.hn as  hn,p.fname as fname,p.lname as lname,p.dob as dob,if(p.gender = 1,"ชาย","หญิง") as gender,p.cid as cid,cn.nationname as nationality,CONCAT(pa.address," ม.",pa.moo," ",pa.subdistrictname," ",pa.districtname," ",pa.provincename) as address,p.tel as tel,cb.bloodgroupname as blood,pd.drug as drugallergy FROM patient p left JOIN view_PatAddressType1 pa ON p.hn = pa.hn  left join c_bloodgroup cb on cb.bloodgroupcode = p.blood left join c_nation cn on p.nationality = cn.nationcode LEFT JOIN pt_drugallergy pd ON p.hn = pd.hn WHERE p.cid = :cid or p.hn = :cid');
+        $this->db->query('SELECT p.hn as  hn,p.fname as fname,p.lname as lname,p.dob as dob,if(p.gender = 1,"ชาย","หญิง") as gender,p.cid as cid,cn.nationname as nationality,CONCAT(pa.address," ม.",pa.moo," ",pa.subdistrictname," ",pa.districtname," ",pa.provincename) as address,p.tel as tel,cb.bloodgroupname as blood,pd.drug as drugallergy 
+,vv.height,vv.weight FROM patient p left JOIN view_PatAddressType1 pa ON p.hn = pa.hn left join c_bloodgroup cb on cb.bloodgroupcode = p.blood left join c_nation cn on p.nationality = cn.nationcode LEFT JOIN pt_drugallergy pd ON p.hn = pd.hn inner join visit v on v.hn = p.hn inner join visit_vitalsign vv on v.id = vv.id and vv.height is not null and vv.weight is not null where (p.cid = :cid or p.hn = :cid) order by v.visitdate limit 1');
         $this->db->bind(':cid',$cid);
         $this->db->execute();
         $dataset = $this->db->single();
